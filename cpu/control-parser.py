fc50e4610054777999f2a551ff20a5094c514f49
Nikolai Lenney <jnlenney>
2019-03-13 12:53:18 -0400

    fleshing out c types, and almost done with python side

---
+++ cpu/control-parser.py
@@ -0,0 +1,459 @@
+'''
+    This tool will help produce important files for the c and sv
+    implementations of our 6502 processor.
+
+    Nikolai Lenney
+
+'''
+
+csv_dir = "control-csv"
+instr_ctrl_signals_file = "instr_ctrl_signals.csv"
+addr_mode_ucode_file = "addr_mode_ucode.csv"
+opcode_mapping_file = "opcode_mappings.csv"
+instr_mem_type_file = "instr_mem_type.csv"
+decode_ctrl_signals_files = "decode_ctrl_signals.csv"
+
+instr_ctrl_signals_struct_name = "instr_ctrl_signals"
+addr_mode_ucode_struct_name = "ucode_ctrl_signals"
+
+
+#ALUOUTDST,ALU1SRC,ALU2SRC,ALU2INV,ALUCINSRC,ALUOP,ZEROSRC,NEGSRC,CARSRC,OVRSRC,INTSRC,BRKSRC,DECSRC,BRANCHBIT,BRANCHINV,STORE
+class instr_ctrl_vector(object):
+    def __init__(self, row):
+        self.init_alu_signals(row[:6])
+        self.init_status_signals(row[6:13])
+        self.init_misc_signals(row[13:])
+
+    def init_alu_signals(self, alu_row):
+        if alu_row[0] == "":
+            self.alu_out_dst = "none"
+        else:
+            self.alu_out_dst = alu_row[0]
+        if alu_row[1] == "":
+            self.alu_src1 = "A"
+        else:
+            self.alu_src1 = alu_row[1]
+        if alu_row[2] == "":
+            self.alu_src2 = "0"
+        else:
+            self.alu_src2 = alu_row[2]
+        if alu_row[3] == "":
+            self.invert_src2 = "0"
+        else:
+            self.invert_src2 = alu_row[3]
+        if alu_row[4] == "":
+            self.alu_c_in = "0"
+        else:
+            self.alu_c_in = alu_row[4]
+        if alu_row[5] == "":
+            self.alu_op = "hold"
+        else:
+            self.alu_op = alu_ops_to_str(alu_row[5])
+
+    def init_status_signals(self, flag_row):
+        if flag_row[0] == "":
+            self.zero_src = "none"
+        else:
+            self.zero_src = flag_row[0]
+        if flag_row[1] == "":
+            self.negative_src = "none"
+        else:
+            self.negative_src = flag_row[1]
+        if flag_row[2] == "":
+            self.carry_src = "none"
+        else:
+            self.carry_src = flag_row[2]
+        if flag_row[3] == "":
+            self.overflow_src = "none"
+        else:
+            self.overflow_src = flag_row[3]
+        if flag_row[4] == "":
+            self.interrupt_src = "none"
+        else:
+            self.interrupt_src = flag_row[4]
+        if flag_row[5] == "":
+            self.break_src = "none"
+        else:
+            self.break_src = flag_row[5]
+        if flag_row[6] == "":
+            self.decimal_src = "none"
+        else:
+            self.decimal_src = flag_row[6]
+
+    def init_misc_signals(self, misc_row):
+        if misc_row[0] == "":
+            self.branch_bit = "C"
+        else:
+            self.branch_bit = misc_row[0]
+        if misc_row[1] == "":
+            self.branch_inv = "0"
+        else:
+            self.branch_inv = misc_row[1]
+        if misc_row[2] == "":
+            self.store_tgt = "A"
+        else:
+            self.store_tgt = misc_row[2]
+
+    def get_flags(self):
+        return [self.negative_src, self.overflow_src, self.break_src,
+                self.decimal_src, self.interrupt_src, self.zero_src,
+                self.carry_src]
+
+    def vector_to_c_struct(self):
+        res = "{"
+        res += "ALUOP_%s, " % self.alu_op
+        res += "ALUDST_%s, " % self.alu_out_dst
+        res += "SRC1_%s, " % self.alu_src1
+        res += "SRC2_%s, " % self.alu_src2
+        res += "Invert_%s, " % self.invert_src2
+        res += "ALUC_%s, " % self.alu_c_in
+        for flag_str in self.get_flags():
+            res += "flag_%s, " % flag_str
+        res += "Branch_%s, " % self.branch_bit
+        res += "Invert_%s, " % self.branch_inv
+        res += "Store_%s" %  self.store_tgt
+        return res + "}"
+
+    def __repr__(self):
+        return self.vector_to_c_struct()
+
+# ADDRLOSRC,ADDRHISRC,R/W,WMEMSRC,   ALU1SRC,ALU2SRC,ALU2INV,ALUCINSRC,ALUOP,   SPSRC,PCLOSRC,PCHISRC,Status,   INCPC,OPCODECTRL,STARTFETCH,STARTDECODE,CSKIP,Stop uCode
+class ucode_vector(object):
+    def __init__(self, fields):
+        self.init_mem_signals(fields[:4])
+        self.init_alu_signals(fields[4:9])
+        self.init_write_reg_signals(fields[9:13])
+        self.init_cpu_ctrl_signals(fields[13:])
+
+    def init_mem_signals(self, mem_row):
+        if mem_row[0] == ""
+            self.addr_lo = "hold"
+        else:
+            self.addr_lo = mem_row[0]
+        if mem_row[1] == "":
+            self.addr_hi = "hold"
+        else:
+            self.addr_hi = mem_row[1]
+        if mem_row[2] == "":
+            self.read_en = "none"
+        else:
+            self.read_en = mem_row[2]
+        if mem_row[3] == "":
+            self.wmem_src = "PCHI"
+        else:
+            self.wmem_src = mem_row[3]
+
+    def init_alu_signals(self, alu_row):
+        if alu_row[0] == "":
+            self.alu_src1 = "A"
+        else:
+            self.alu_src1 = alu_row[0]
+        if alu_row[1] == "":
+            self.alu_src2 = "0"
+        else:
+            self.alu_src2 = alu_row[1]
+        if alu_row[2] == "":
+            self.invert_src2 = "0"
+        else:
+            self.invert_src2 = alu_row[2]
+        if alu_row[3] == "":
+            self.alu_c_in = "0"
+        else:
+            self.alu_c_in = alu_row[3]
+        if alu_row[4] == "":
+            self.alu_op = "hold"
+        else:
+            self.alu_op = alu_ops_to_str(alu_row[4])
+
+    def init_write_reg_signals(self, write_reg_row):
+        if write_reg_row[0] == "":
+            self.write_sp = "none"
+        else:
+            self.write_sp = write_reg_row[0]
+        if write_reg_row[1] == "":
+            self.write_pclo = "none"
+        else:
+            self.write_pclo = write_reg_row[1]
+        if write_reg_row[2] == "":
+            self.write_pchi = "none"
+        else:
+            self.write_pchi = write_reg_row[2]
+        if write_reg_row[3] == "":
+            self.write_status = "none"
+        else:
+            self.write_status = write_reg_row[3]
+
+    def init_cpu_ctrl_signals(self, cpu_ctrl_row):
+        if cpu_ctrl_row[0] == "":
+            self.inc_pc = "0"
+        else:
+            self.inc_pc = cpu_ctrl_row[0]
+        if cpu_ctrl_row[1] == "":
+            self.instr_ctrl = "0"
+        else:
+            self.instr_ctrl = cpu_ctrl_row[1]
+        if cpu_ctrl_row[2] == "":
+            self.start_fetch = "0"
+        else:
+            self.start_fetch = cpu_ctrl_row[2]
+        if cpu_ctrl_row[3] == "":
+            self.start_decode = "0"
+        else:
+            self.start_decode = cpu_ctrl_row[3]
+        if cpu_ctrl_row[4] == "":
+            self.carry_skip = "0"
+        else:
+            self.carry_skip = cpu_ctrl_row[4]
+        if cpu_ctrl_row[5] == "":
+            self.stop_ucode = "0"
+        else:
+            self.stop_ucode = cpu_ctrl_row[5]
+
+    def vector_to_c_struct(self):
+        res = "{"
+        res += "ADDRLO_%s, " % self.addr_lo
+        res += "ADDRHI_%s, " % self.addr_hi
+        res += "ReadEn_%s, " % self.read_en
+        res += "WMEMSRC_%s, " % self.wmem_src
+        res += "SRC1_%s, " % self.alu_src1
+        res += "SRC2_%s, " % self.alu_src2
+        res += "Invert_%s, " % self.invert_src2
+        res += "ALUC_%s, " % self.alu_c_in
+        res += "ALUOP_%s, " % self.alu_op
+        res += "SPSRC_%s, " % self.write_sp
+        res += "PCLO_%s, " % self.write_pclo
+        res += "PCHI_%s, " % self.write_pchi
+        res += "Status_SRC_%s, " % self.write_status
+        res += "Branch_Depend_%s, " % self.inc_pc
+        res += "INSTR_CTRL_%s, " % self.instr_ctrl
+        res += "Enable_%s, " % self.start_fetch
+        res += "Branch_Depend_%s, " % self.start_decode
+        res += "Enable_%s, " % self.carry_skip
+        res += "Branch_Depend_%s" % self.stop_ucode
+        return res + "}"
+
+    def __repr__(self):
+        return self.vector_to_c_struct()
+
+class decode_ctrl_signal_vector(object):
+    def __init__(self, row):
+        if row[0] == "":
+            self.inc_pc = "0"
+        else:
+            self.inc_pc = row[0]
+        if row[1] == "":
+            self.start_fetch = "0"
+        else:
+            self.start_fetch = row[1]
+
+    def to_packed_int(self):
+        res = 0
+        # lowest bit is inc_pc
+        res += 1 * int(self.inc_pc)
+        # next lowest bit is begin fetch
+        res += 2 * int(self.start_fetch)
+        return res
+
+
+default_instr_ctrl_vector = instr_ctrl_vector([""]*16)
+
+default_ucode_vector = ucode_vector([""]*18+["1"])
+
+default_decode_ctrl_signal_vector = decode_ctrl_signal_vector([""]*2)
+
+def alu_ops_to_str(op):
+    alu_op_dict = {"+" : "add", "^" : "xor", "&" : "and", "|" : "or",
+                   ">>" : "right_shift", "<<" : "left_shift"}
+    return alu_op_dict[op]
+
+# these two functions were taken from CMU 15-112 website
+# https://www.cs.cmu.edu/~112/notes/notes-strings.html#basicFileIO
+def readFile(path):
+    with open(path, "rt") as f:
+        return f.read()
+
+def writeFile(path, contents):
+    with open(path, "wt") as f:
+        f.write(contents)
+
+# takes in a string which is the raw output of reading a csv
+def csv_to_2d_list(contents):
+    res = []
+    for line in contents.splitlines():
+        row = line.split(',')
+        res.append(row)
+    return res
+
+def pp_2d_list(L):
+    print("[")
+    for row in L:
+        print(row, end=",\n")
+    print("]")
+
+def get_instr_ctrl_signals():
+    path = "%s/%s" % (csv_dir, instr_ctrl_signals_file)
+    contents = readFile(path)
+    instr_ctrl_signals_list = csv_to_2d_list(contents)
+    # remove the header row
+    instr_ctrl_signals_list.pop(0)
+    
+    instr_indices = dict()
+    instr_ctrl_signals = [default_instr_ctrl_vector]
+    for row in instr_ctrl_signals_list:
+        instr = row[0]
+        # skip the second field sinse it is just describes the instr
+        fields = row[2:]
+        instr_indices[instr] = len(instr_ctrl_signals)
+        instr_ctrl_signals.append(instr_ctrl_vector(fields))
+
+    return instr_indices, instr_ctrl_signals
+
+
+def get_ucode():
+    path = "%s/%s" % (csv_dir, addr_mode_ucode_file)
+    contents = readFile(path)
+    ucode_csv_list = csv_to_2d_list(contents)
+    # remove the header row
+    ucode_csv_list.pop(0)
+
+    ucode_indices = dict()
+    ucode_rom = [default_ucode_vector]
+
+    for row in ucode_csv_list:
+        addr_mode = row[0]
+        if addr_mode != "":
+            ucode_indices[addr_mode] = len(ucode_rom)
+        fields = row[1:]
+        ucode_rom.append(ucode_vector(fields))
+
+    return ucode_indices, ucode_rom
+
+
+def get_instr_to_mem_type_dict():
+    path = "%s/%s" % (csv_dir, instr_mem_type_file)
+    contents = readFile(path)
+    instr_mem_type_list = csv_to_2d_list(contents)
+    instr_mem_type_list.pop(0)
+
+    res = dict()
+
+    for row in instr_mem_type_list:
+        res[row[0]] = row[1]
+
+    return res
+
+def get_opcode_dicts():
+    instr_mem_type_dict = get_instr_to_mem_type_dict()
+    path = "%s/%s" % (csv_dir, opcode_mapping_file)
+    contents = readFile(path)
+    opcode_mappings = csv_to_2d_list(contents)
+    opcode_mappings.pop(0)
+
+    instr_dict = dict()
+    addr_mode_dict = dict()
+
+    for row in opcode_mappings:
+        opcode = row[2]
+        instr = row[0]
+        if instr in instr_mem_type_dict:
+            addr_mode = row[1] + instr_mem_type_dict[instr]
+        else:
+            addr_mode = row[1]
+
+        instr_dict[opcode] = instr
+        addr_mode_dict[opcode] = addr_mode
+
+        return instr_dict, addr_mode_dict
+
+def get_opcode_to_decode_ctrl_signals(opcode_to_addr_mode):
+    path = "%s/%s" % (csv_dir, decode_ctrl_signals_files)
+    contents = readFile(path)
+    decode_ctrl_signals_list = csv_to_2d_list(contents)
+    decode_ctrl_signals_list.pop(0)
+
+    addr_mode_to_decode_ctrl_signals_dict = dict()
+    for row in decode_ctrl_signals_list:
+        addr_mode = row[0]
+        decode_ctrl_signals = decode_ctrl_signals_vector(row[1:])
+        addr_mode_to_decode_ctrl_signals_dict[addr_mode] = decode_ctrl_signals
+
+    opcode_to_decode_ctrl_signals = dict()
+    for opcode in opcode_to_addr_mode:
+        addr_mode = opcode_to_addr_mode[opcode]
+        decode_ctrl_signals = addr_mode_to_decode_ctrl_signals_dict[addr_mode]
+        opcode_to_decode_ctrl_signals[opcode] = decode_ctrl_signals
+
+    return opcode_to_decode_ctrl_signals
+
+def get_c_struct_array(struct_name, vector_list):
+    res = "%s %s_rom[] = {\n" % (struct_name, struct_name)
+    for vector in vector_list:
+        res += "  %s,\n" % str(vector)
+    return res + "}\n"
+
+def get_hex_byte(i):
+    hex_chrs = "0123456789ABCDEF"
+    i %= 256
+    upper_nibble, lower_nibble = i//16, i % 16
+    return "0x%s%s" % (hex_chrs[upper_nibble], hex_chrs[lower_nibble])
+
+def get_c_indices(struct_name, opcode_dict, index_dict):
+    res = "uint8_t %s_indices[] = {" % (struct_name)
+    ct = 0
+
+    for i in range(256):
+        hex_i = get_hex_byte(i)
+        if hex_i in opcode_dict:
+            target_value = opcode_dict[hex_i]
+            if target_value in index_dict:
+                index = index_dict[target_value]
+            else:
+                index = "0"
+        else:
+            index = "0"
+
+        if ct == 15:
+            res += "%d,\n" % index
+            ct = 0
+        else:
+            res += "%d, " % index
+            ct += 1
+
+    return res + "}\n"
+
+def get_c_decode_ctrl_signals_array(opcode_to_decode_ctrl_signals):
+    res = "uint8_t decode_ctrl_signals_rom[] = {"
+
+    for i in range(256):
+        hex_i = get_hex_byte(i)
+        if hex_i in opcode_to_decode_ctrl_signals:
+            decode_ctrl_signals_byte = str(opcode_to_decode_ctrl_signals[hex_i])
+        else:
+            decode_ctrl_signals_byte = str(default_decode_ctrl_signal_vector)
+
+        if ct == 15:
+            res += "%d,\n" % decode_ctrl_signals_byte
+            ct = 0
+        else:
+            res += "%d, " % decode_ctrl_signals_byte
+            ct += 1
+
+    return res + "}\n"
+
+def main():
+    instr_indices, instr_ctrl_signals = get_instr_ctrl_signals()
+    #print(instr_ctrl_signals)
+    ucode_indices, ucode_rom = get_ucode()
+    opcode_to_instr, opcode_to_addr_mode = get_opcode_dicts()
+    opcode_to_decode_ctrl_signals = get_opcode_to_decode_ctrl_signals(opcode_to_addr_mode)
+
+    instr_ctrl_signals_vector_array = get_c_struct_array(instr_ctrl_signals_struct_name, instr_ctrl_signals)
+    ucode_vector_array = get_c_struct_array(addr_mode_ucode_struct_name, ucode_rom)
+
+    instr_ctrl_signals_indices_array = get_c_indices_array(instr_ctrl_signals_struct_name, opcode_to_instr, instr_indices)
+    ucode_indices_array = get_c_indices_array(addr_mode_ucode_struct_name, opcode_to_addr_mode, ucode_indices)
+    decode_ctrl_signals_array = get_c_decode_ctrl_signals_array(opcode_to_decode_ctrl_signals)
+
+    # how to write to another file? will need to put the structs and enums in their own file?
+
+main()
---
+++ cpu/cpu-types.h
@@ -0,0 +1,94 @@
+
+#include <stdint.h>
+
+// enums used for instr ctrl signals
+
+typedef enum {ALUOP_hold, ALUOP_add, ALUOP_and, ALUOP_or, ALUOP_xor, ALUOP_shift_left, ALUOP_shift_right} ctrl_alu_op;
+
+typedef enum {ALUDST_A, ALUDST_X, ALUDST_Y, ALUDST_WMEM, ALUDST_status, ALUDST_SP, ALUDST_none} ctrl_alu_out_dst;
+
+typedef enum {SRC1_A, SRC1_X, SRC1_Y, SRC1_RMEM, SRC1_SP, SRC1_PCHI, SRC1_PCLO, SRC1_ALUOUT} ctrl_alu_src1;
+
+typedef enum {SRC2_RMEM, SRC2_0} ctrl_alu_src2;
+
+typedef enum {Invert_1, Invert_0} ctrl_inv;
+
+typedef enum {ALUC_C, ALUC_0, ALUC_1, ALUC_ALUCOUT} ctrl_alu_c_src;
+
+typedef enum {Flag_alu, Flag_0, Flag_1, Flag_none} ctrl_flag_src;
+
+typedef enum {Branch_C, Branch_Z, Branch_N, Branch_V} ctrl_branch_bit;
+
+typedef enum {Store_A, Store_X, Store_Y, Store_status} ctrl_store_reg;
+
+// enums used for addressing mode ucode
+
+typedef enum {ADDRLO_1, ADDRLO_FF, ADDRLO_PCLO, ADDRLO_RMEMBUFFER, ADDRLO_0, ADDRLO_ALUOUT, ADDRLO_hold} ucode_addr_lo_src;
+
+typedef enum {ADDRHI_SP, ADDRHI_FE, ADDRHI_FF, ADDRHI_PCHI, ADDRHI_RMEM, ADDRHI_ALUOUT, ADDRHI_hold} ucode_addr_hi_src;
+
+typedef enum {ReadEn_R, ReadEn_W, ReadEn_none} ucode_r_en;
+
+typedef enum {WMEMSRC_PCHI, WMEMSRC_PCLO, WMEMSRC_status, WMEMSRC_instr_store, WMEMSRC_RMEM} ucode_write_mem_src;
+
+typedef enum {SPSRC_ALUOUT, SPSRC_none} ucode_sp_src;
+
+typedef enum {PCLOSRC_RMEM, PCLOSRC_ALUOUT, PCLOSRC_RMEM_BUFFER, PCLOSRC_none} ucode_pclo_src;
+
+typedef enum {PCHISRC_RMEM, PCHISRC_ALUOUT, PCHISRC_none} ucode_pchi_src;
+
+typedef enum {Status_SRC_RMEM, Status_SRC_none} ucode_status_src;
+
+typedef enum {Branch_Depend_0, Branch_Depend_1, Branch_Depend_branch_bit, Branch_Depend_not_c_out} ucode_branch_depend;
+
+typedef enum {INSTR_CTRL_0, INSTR_CTRL_1, INSTR_CTRL_2} ucode_instr_ctrl;
+
+typedef enum {Enable_1, Enable_0} ucode_en;
+
+// enum for the states of the processor overall
+
+typedef enum {State_fetch, State_decode, State_neither} processor_state;
+
+typedef enum {State_ucode_active, State_ucode_inactive} processor_ucode_activity;
+
+typedef struct {
+    ctrl_alu_op alu_op;
+    ctrl_alu_out_dst alu_out_dst;
+    ctrl_alu_src1 alu_src1;
+    ctrl_alu_src2 alu_src2;
+    ctrl_inv alu_src2_inv;
+    ctrl_alu_c_src alu_c_src;
+    ctrl_flag_src n_src;
+    ctrl_flag_src v_src;
+    ctrl_flag_src b_src;
+    ctrl_flag_src d_src;
+    ctrl_flag_src i_src;
+    ctrl_flag_src z_src;
+    ctrl_flag_src c_src;
+    ctrl_branch_bit branch_bit;
+    ctrl_inv branch_inv;
+    ctrl_store_reg store_reg;
+} instr_ctrl_signals;
+
+typedef struct 
+{
+    ucode_addr_lo_src addr_lo_src;
+    ucode_addr_hi_src addr_hi_src;
+    ucode_r_en r_en;
+    ucode_write_mem_src write_mem_src;
+    ctrl_alu_src1 alu_src1;
+    ctrl_alu_src2 alu_src2;
+    ctrl_alu_src2_inv alu_src2_inv;
+    ctrl_alu_c_src alu_c_src;
+    ctrl_alu_op alu_op;
+    ucode_sp_src sp_src;
+    ucode_pclo_src pclo_src;
+    ucode_plhi_src pchi_src;
+    ucode_status_src status_src;
+    ucode_branch_depend inc_pc;
+    ucode_instr_ctrl instr_ctrl;
+    ucode_en start_fetch;
+    ucode_branch_depend start_decode;
+    ucode_en skip_line;
+    ucode_branch_depend stop_ucode;
+} ucode_ctrl_signals;
