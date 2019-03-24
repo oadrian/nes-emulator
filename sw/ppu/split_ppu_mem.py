1f896f802a460ba0d5b08d74ae609b4c17cd1593
Oscar Ramirez Poulat <oscar.poulat@gmail.com>
2019-03-24 19:21:16 -0400

    added a script to test the frame difference between my generated frames (sw and hw) and reference frame

--- sw/ppu/split_ppu_mem.py
+++ sw/ppu/split_ppu_mem.py
@@ -8,15 +8,19 @@ NAMETBL_FILE = "init/vram_init.txt"
 PAL_FILE = "init/pal_init.txt"
 OAM_FILE = "init/oam_init.txt"
 
-def main():
-    filename = sys.argv[1]
+def split(filename, pattbl_fl, nametbl_fl, pal_fl, oam_fl):
     pattbl, nametbl, pal, oam = trace_lib.parseMemoryFile(filename)
     vram = nametbl[0:0x800]  # only first two nametables
 
-    trace_lib.dumpMemory(PATTBL_FILE, pattbl)
-    trace_lib.dumpMemory(NAMETBL_FILE, vram)
-    trace_lib.dumpMemory(PAL_FILE, pal)
-    trace_lib.dumpMemory(OAM_FILE, oam)
+    trace_lib.dumpMemory(pattbl_fl, pattbl)
+    trace_lib.dumpMemory(nametbl_fl, vram)
+    trace_lib.dumpMemory(pal_fl, pal)
+    trace_lib.dumpMemory(oam_fl, oam)
+
+def main():
+    filename = sys.argv[1]
+    split(filename, PATTBL_FILE, NAMETBL_FILE, PAL_FILE, OAM_FILE)
+    
 
 if __name__ == '__main__':
     main()
---
+++ sw/ppu/test_frame_diff.py
@@ -0,0 +1,70 @@
+import sys
+import os
+import subprocess
+import frame_sw_sim as sw_sim 
+import trace_lib as lib 
+import split_ppu_mem as splitter
+import frame_gen as hw_gen
+
+def getTraceNames(trace_folder):
+    directory = os.fsencode(trace_folder)
+    traces = []
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if(filename.endswith(".txt")):
+            trace = filename[:-4]
+            traces.append(trace)
+    return traces
+
+def testSW(traces, trace_folder):
+    for trace in traces:
+        print("testing trace: " + trace)
+        txt_path = trace_folder+ "/" + trace+".txt"
+        png_path = trace_folder+ "/" + trace+".png"
+        vram, palette_ram, oam = sw_sim.parseMemory(txt_path)
+        bitmap0 = sw_sim.createBitmap(vram, palette_ram, oam)
+        bitmap1 = lib.image2Bitmap(png_path)
+        good, total = lib.compareBitmaps(bitmap0, bitmap1)
+        print("Matched: "+str(100*good/total)+"% (" + str(good) + "/" + str(total) + ") of pixels\n\n")
+
+def testHW(traces, trace_folder):
+    pattbl_fl = "init/chr_rom_init.txt"
+    nametbl_fl = "init/vram_init.txt"
+    pal_fl = "init/pal_init.txt"
+    oam_fl = "init/oam_init.txt"
+    for trace in traces:
+        print("testing trace: " + trace)
+        txt_path = trace_folder+ "/" + trace+".txt"
+        png_path = trace_folder+ "/" + trace+".png"
+
+        # split ppu mem to init/ folder for hw simulation
+        splitter.split(txt_path, pattbl_fl, nametbl_fl, pal_fl, oam_fl)
+
+        # run ./simv 
+        subprocess.run(["./simv"])
+
+        # convert generated frame data to bitmap
+        bitmap0 = hw_gen.createBitmap("my_frame.txt")
+        bitmap1 = lib.image2Bitmap(png_path)
+        good, total = lib.compareBitmaps(bitmap0, bitmap1)
+        print("Matched: "+str(100*good/total)+"% (" + str(good) + "/" + str(total) + ") of pixels\n\n")
+
+def main():
+    target = sys.argv[1]
+    trace_folder = sys.argv[2]
+    traces = getTraceNames(trace_folder)
+    if(target == "sw"):
+        print("----Testing Software Script----\n\n")
+        testSW(traces, trace_folder)
+    elif(target == "hw"):
+        pass
+    elif(target == "all"):
+        print("----Testing ALL----\n\n")
+        print("----Testing Software Script----\n\n")
+        testSW(traces, trace_folder)
+    else:
+        print("Did not recognized target "+target)
+        exit(1)
+
+if __name__ == '__main__':
+    main()
