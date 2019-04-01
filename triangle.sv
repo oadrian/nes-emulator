b30c32f11c811c7984eb6bcd3250285b4ee8138b
Diego Rodriguez <darodrig@helios.andrew.local.cmu.edu>
2019-03-31 21:34:04 -0400

    Implemented the length counter

--- triangle.sv
+++ triangle.sv
@@ -3,35 +3,25 @@
 module triangle_channel (
   input logic clk, rst_l,
   input logic halt,
+  input logic load,
   input logic [6:0] linear_cnt_load,
-  input logic [10:0] timer,
+  input logic [10:0] period,
   input logic [4:0] length_cnt_load,
   output logic [3:0] out);
 
 
   logic [3:0] seq_out;
+  logic timer_pulse;
+  logic [31:0][3:0] seq;
+  logic [4:0] seq_i;
 
-  register #(.WIDTH(4'h4), .RES_VAL(4'hF)) seq_reg (
-    .clk, .rst_l, .en(1'b1), .d(seq_out), .q(out));
+  assign seq = 128'hFEDCBA98765432100123456789ABCDEF;
+  assign out = seq[seq_i];
+  
 
-  // Sequencer
-  always_ff @(posedge length_gate_out, negedge rst_l)
-    if (~rst_l)
-      seq_out <= 15
-      
+
+  linear_down_counter #(.WIDTH(11), .RES_VAL(0)) timer (
+    .clk, .rst_l, .load_val(period), .load, .pulse(timer_pulse));      
 
 endmodule: triangle_channel
 
-module register #(parameter WIDTH=32, RES_VAL=0)(
-  input logic clk, rst_l,
-  input logic en,
-  input logic [WIDTH-1:0] d,
-  output logic [WIDTH-1:0] q);
-
-  always_ff @(posedge clk, negedge rst_l)
-    if (~rst_l)
-      q <= RES_VAL;
-    else if (en)
-      q <= d;
-
-endmodule: register
