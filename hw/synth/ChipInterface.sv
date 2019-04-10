0a7fee0617db1d99b3647aa53565c728e354fd33
Oscar Ramirez Poulat <oscar.poulat@gmail.com>
2019-04-10 18:38:14 -0400

    Controller kinda works, need to fix timing probably

--- hw/synth/ChipInterface.sv
+++ hw/synth/ChipInterface.sv
@@ -7,11 +7,16 @@ module ChipInterface
   (input  logic CLOCK_50, 
    input  logic [3:0] KEY, 
    input  logic [17:0] SW, 
+   // HEX displays
    output logic [6:0] HEX0, HEX1, HEX2, HEX3,  
                       HEX4, HEX5, HEX6, HEX7, 
+   // VGA
    output logic [7:0]  VGA_R, VGA_G, VGA_B, 
    output logic        VGA_BLANK_N, VGA_CLK, VGA_SYNC_N, 
-   output logic        VGA_VS, VGA_HS); 
+   output logic        VGA_VS, VGA_HS,
+   // GPIO PINS
+   inout logic [35:0] GPIO
+   ); 
 
 	logic reset_n;
     assign reset_n = SW[17];
@@ -110,37 +115,37 @@ module ChipInterface
     assign mem_rd_data_c = mem_rd_data;
     assign mem_rd_data_p = mem_rd_data;
 	 
-	logic up, down, start, select, left, right, A, B;
+	// logic up, down, start, select, left, right, A, B;
      
-     always_ff @(posedge clock or negedge reset_n) begin
-        if(~reset_n) begin
-            up <= 0;
-            down <= 0;
-            start <= 0;
-            select <= 0;
+ //     always_ff @(posedge clock or negedge reset_n) begin
+ //        if(~reset_n) begin
+ //            up <= 0;
+ //            down <= 0;
+ //            start <= 0;
+ //            select <= 0;
 
-            left <= 0;
-            right <= 0;
-            A <= 0;
-            B <= 0;
-        end else begin
-            up <= ~KEY[3] && SW[0];
-            down <= ~KEY[2] && SW[0];
-            start <= ~KEY[1] && SW[0];
-            select <= ~KEY[0] && SW[0];
+ //            left <= 0;
+ //            right <= 0;
+ //            A <= 0;
+ //            B <= 0;
+ //        end else begin
+ //            up <= ~KEY[3] && SW[0];
+ //            down <= ~KEY[2] && SW[0];
+ //            start <= ~KEY[1] && SW[0];
+ //            select <= ~KEY[0] && SW[0];
 
-            left <= ~KEY[3] && ~SW[0];
-            right <= ~KEY[2] && ~SW[0];
-            A <= ~KEY[1] && ~SW[0];
-            B <= ~KEY[0] && ~SW[0];
-        end
-     end
+ //            left <= ~KEY[3] && ~SW[0];
+ //            right <= ~KEY[2] && ~SW[0];
+ //            A <= ~KEY[1] && ~SW[0];
+ //            B <= ~KEY[0] && ~SW[0];
+ //        end
+ //     end
 
     cpu_memory mem(.addr(mem_addr), .r_en(mem_re), .w_data(mem_wr_data), 
                    .clock, .clock_en(cpu_clk_en), .reset_n, .r_data(mem_rd_data), 
                    .reg_sel, .reg_en, .reg_rw, .reg_data_wr, .reg_data_rd,
                    .read_prom,
-                   .up, .down, .start, .select, .left, .right, .A, .B);
+                   .ctlr_pulse(GPIO[26]), .ctlr_latch(GPIO[28]), .ctlr_data(GPIO[30]));
 
 
     // see ppu status registers
