77484be041de568d610b98eda0073a2beff324d4
Diego Rodriguez <diegoarodriguez741@gmail.com>
2019-04-22 23:28:15 -0400

    Merge

--- hw/apu/frame_counter.sv
+++ hw/apu/frame_counter.sv
@@ -10,19 +10,55 @@ module frame_counter (
   output logic interrupt,
   output logic quarter_clk_en, half_clk_en);
 
-  logic reload;
-  logic [15:0] num_cycles;
+  logic [15:0] next_num_cycles, num_cycles;
+//TODO: The interrupt should be turned off sometimes
 
-  up_counter #(.WIDTH(16), .RES_VAL(0)) cycle_counter (
-    .clk, .rst_l, .clk_en(cpu_clk_en), .en(1'b1), .load(reload),
-    .load_data(16'b0), .count(num_cycles));
+  apu_register #(.WIDTH(16), .RES_VAL(0)) cycles_reg (
+    .clk, .rst_l, .clk_en(cpu_clk_en), .en(1'b1),
+    .d(next_num_cycles), .q(num_cycles));
 
+/*
+  always_ff @(posedge clk or negedge rst_l) begin : proc_
+    if(~rst_l) begin
+      quarter_clk_en <= 1'b0;
+      half_clk_en <= 1'b0;
+      interrupt <= 1'b0;
+    end else begin
+      quarter_clk_en <= 1'b0;
+      half_clk_en <= 1'b0;
+      interrupt <= 1'b0;
+
+      if (cpu_clk_en) begin
+
+        if (num_cycles == 16'd7457)
+            quarter_clk_en <= 1'b1;
+        else if (num_cycles == 16'd14913)
+          {quarter_clk_en, half_clk_en} <= 2'b11;
+        else if (num_cycles == 16'd22371)
+          quarter_clk_en <= 1'b1;
+        else if (~mode && num_cycles == 16'd29829) begin
+          {quarter_clk_en, half_clk_en} <= 2'b11;
+          interrupt <= ~inhibit_interrupt;
+          num_cycles <= 16'b0;          
+        end else if (num_cycles == 16'd37281) begin
+          {quarter_clk_en, half_clk_en} <= 2'b11;
+          num_cycles <= 16'b0;
+        end       
+        if (load)
+          num_cycles <= 16'd37281;
+        else
+          num_cycles <= num_cycles + 1'b1;
+
+      end
+    end
+  end
+*/
 
   always_comb begin
     quarter_clk_en = 1'b0;
     half_clk_en = 1'b0;
     interrupt = 1'b0;
-    reload = 1'b0;
+    next_num_cycles = num_cycles + 1'b1;
 
     if (num_cycles == 16'd7457)
       quarter_clk_en = 1'b1;
@@ -33,16 +69,14 @@ module frame_counter (
     else if (~mode && num_cycles == 16'd29829) begin
       {quarter_clk_en, half_clk_en} = 2'b11;
       interrupt = ~inhibit_interrupt;
-      reload = 1'b1;
-    end else if (mode && num_cycles == 16'd37281) begin
+      next_num_cycles = 16'b0;
+    end else if (num_cycles == 16'd37281) begin
       {quarter_clk_en, half_clk_en} = 2'b11;
-      reload = 1'b1;
+      next_num_cycles = 16'b0;
     end
-
-    reload = load ? 1'b1 : reload;
       
-    if (load & mode)
-      {quarter_clk_en, half_clk_en} = 2'b11;
+    if (load)
+      next_num_cycles = 16'd37281;
   end
 
 endmodule: frame_counter
